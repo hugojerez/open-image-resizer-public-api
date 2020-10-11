e417724bc0638e62e191f0ddc302f44109c65ecf
hugo <hugo@MacBook-Pro-de-hugo.local>
2020-10-11 20:59:54 -0300

    port change

--- index.js
+++ index.js
@@ -5,7 +5,7 @@ const fs = require("fs")
 const resizeImg = require("resize-img")
 const express = require("express")
 const app = express()
-const port = 3000
+const port = 80
 const {promisify} = require("util")
 const writeFilePromise = promisify(fs.writeFile)
 
